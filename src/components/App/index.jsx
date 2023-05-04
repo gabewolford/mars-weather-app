e60af5716d68abb280495e1271dead87ea3d79ec
gabewolford <gabewolford@gmail.com>
2023-05-04 10:31:49 -0700

    API is down indefinitely

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,18 @@
+import { useState, useEffect } from 'react'
 import './styles.css'
 
 function App() {
+  const [weather, setWeather] = useState([])
+  
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(`https://api.nasa.gov/insight_weather/?api_key=${import.meta.env.VITE_API_KEY}&feedtype=json&ver=1.0`)
+      const data = await res.json()
+      console.log(data)
+    }
+
+    getData()
+  })
 
   return (
     <>
